eacf3cbc1ea5809b21a6f0f3fbbbefb5732faf5d
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-06-09 13:58:31 -0500

    create them all in one go

--- data/circos_links_files.py
+++ data/circos_links_files.py
@@ -2,25 +2,26 @@ import csv
 
 from gene_positions import gene
 
-with open('TablaParaCircosSanosEdge.csv') as fi, \
-     open('links_sanos_same_chr.tsv', 'w') as fsame, \
-     open('links_sanos_diff_chr.tsv', 'w') as fdiff:
-    csvr     = csv.DictReader(fi)
-    same_chr = csv.writer(fsame, delimiter='\t')
-    diff_chr = csv.writer(fdiff, delimiter='\t')
+for status in ('sanos', 'enfermos'):
+    with open('%s_edges.csv' % status) as fi, \
+         open('links_%s_same_chr.tsv' % status, 'w') as fsame, \
+         open('links_%s_diff_chr.tsv' % status, 'w') as fdiff:
+        csvr     = csv.DictReader(fi)
+        same_chr = csv.writer(fsame, delimiter='\t')
+        diff_chr = csv.writer(fdiff, delimiter='\t')
 
-    for row in csvr:
-        (s,w,t)=row['name'].split()
-        try:
-            source = gene[s]
-            target = gene[t]
-        except:
-            continue
+        for row in csvr:
+            (s,w,t)=row['name'].split()
+            try:
+                source = gene[s]
+                target = gene[t]
+            except:
+                continue
 
-        if source[2] == target[2]:
-            same_chr.writerow(["hs%s" % source[2], source[0], source[1], "hs%s" % target[2], target[0], target[1]])
-        else:
-            diff_chr.writerow(["hs%s" % source[2], source[0], source[1], "hs%s" % target[2], target[0], target[1]])
+            if source[2] == target[2]:
+                same_chr.writerow(["hs%s" % source[2], source[0], source[1], "hs%s" % target[2], target[0], target[1]])
+            else:
+                diff_chr.writerow(["hs%s" % source[2], source[0], source[1], "hs%s" % target[2], target[0], target[1]])
 
 
 
