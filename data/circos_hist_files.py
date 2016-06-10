909297f1577bc24f44cf367fbf2c43ac1a43b8ad
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-06-10 12:03:47 -0500

    different degrees by phenotype

--- data/circos_hist_files.py
+++ data/circos_hist_files.py
@@ -1,16 +1,8 @@
-
-# Will create this
-
-# hs6 30844198 30867933 0.949201096539538
-# hs7 73645829 73668774 0.529426928008456
-#hs1 161576081 161578007 -0.319124653818656
-
-
 import csv
 from gene_positions import gene
 
 with open('enfermos_nodes.csv') as f, \
-     open('hist_degree.tsv', 'w') as deg, \
+     open('hist_enfermos_degree.tsv', 'w') as deg, \
      open('hist_lratio.tsv', 'w') as rat:
         csvr     = csv.DictReader(f)
         degw = csv.writer(deg, delimiter='\t')
@@ -30,3 +22,21 @@ with open('enfermos_nodes.csv') as f, \
 
             degw.writerow(["hs%s" % chrom, start, end, degree])
             ratw.writerow(["hs%s" % chrom, start, end, lratio])
+
+
+
+with open('sanos_nodes.csv') as f, \
+     open('hist_sanos_degree.tsv', 'w') as deg:
+        csvr     = csv.DictReader(f)
+        degw = csv.writer(deg, delimiter='\t')
+
+        for row in csvr:
+            try:
+                (start, end, chrom) = gene[row['shared name']]
+                degree = row['Node Degree']
+                if degree == "":
+                    degree = 0
+            except:
+                continue            
+
+            degw.writerow(["hs%s" % chrom, start, end, degree])            
